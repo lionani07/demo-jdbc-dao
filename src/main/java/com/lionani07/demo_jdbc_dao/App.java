8943d073d8783a25e7fad42c1aaea27e9849420b
Liorge Corria Sanchez <lionani07@gmail.com>
2020-03-07 19:36:27 -0300

    Create repository

---
+++ src/main/java/com/lionani07/demo_jdbc_dao/App.java
@@ -0,0 +1,13 @@
+package com.lionani07.demo_jdbc_dao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
